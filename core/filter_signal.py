7e6b7eea2ba03488d99e722f71c992f58828e15a
Marcelo Dourado <marcelodourado.eq@gmail.com>
2025-12-10 14:43:26 -0300

    a

--- core/filter_signal.py
+++ core/filter_signal.py
@@ -8,5 +8,5 @@ def filtrar_sinal(dataframe):
     y_filter = savgol_filter(x=dataframe[dataframe.columns[1]], window_length=4, polyorder=3, deriv=2)#, cval=dataframe[dataframe.columns[1]].median())
     baseline_ = baseline(y=dataframe[dataframe.columns[1]] , deg=3, tol=1e-3, max_it=1000)
     
-    dataframe.loc[:, "Filtered"] = dataframe[dataframe.columns[1]] - baseline_
+    dataframe.loc[:, "Filtered"] = y_filter - baseline_
     return dataframe

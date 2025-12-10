741df055ad8f841a69d4a37894612aadd4b1de73
Marcelo Dourado <marcelodourado.eq@gmail.com>
2025-12-10 14:43:26 -0300

    a

--- core/filter_signal.py
+++ core/filter_signal.py
@@ -6,9 +6,7 @@ import streamlit as st
 def filtrar_sinal(dataframe):
 
     y_filter = savgol_filter(x=dataframe[dataframe.columns[1]], window_length=4, polyorder=3, deriv=2)#, cval=dataframe[dataframe.columns[1]].median())
-    # baseline_ = baseline(y=dataframe[dataframe.columns[1]] , deg=2, tol=1e-3, max_it=1000)
-    baseline_ = baseline(y_filter, deg=2, tol=1e-3, max_it=1000)
-
+    baseline_ = baseline(y=dataframe[dataframe.columns[1]] , deg=3, tol=1e-3, max_it=1000)
     
     dataframe.loc[:, "Filtered"] = dataframe[dataframe.columns[1]] - baseline_
     return dataframe
--- core/find_abs.py
+++ core/find_abs.py
@@ -19,7 +19,7 @@ def achar_abs(dataframe, pico, grafico=True):
         fig, ax = plt.subplots()
 
         sns.lineplot(x=dataframe_processado.columns[0], y=dataframe_processado.columns[-1], data=dataframe_processado)
-        ax.set_ylim(0, 2)
+        ax.set_ylim(-0.01, 2.0)
 
         st.pyplot(fig)
         # st.line_chart(data=dataframe_processado, x=dataframe_processado.columns[0], y=dataframe_processado.columns[-1], y_label="Absorbância")

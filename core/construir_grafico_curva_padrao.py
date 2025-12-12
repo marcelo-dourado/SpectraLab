d7b790573108278f5bb0c9c9d3c2cf62b66c1cd1
Marcelo Dourado <marcelodourado.eq@gmail.com>
2025-12-12 10:24:35 -0300

    troquei sintaxe do matplotlib do construir_grafico_curva_padrao

--- core/construir_grafico_curva_padrao.py
+++ core/construir_grafico_curva_padrao.py
@@ -7,13 +7,13 @@ def construir_grafico_curva_padrao(curva_elemento, modelo_regressao_linear, r2_a
     X = curva_elemento["Concentração (g/L)"].to_numpy().reshape(-1, 1)
     y_predicted = modelo_regressao_linear.predict(X)
     
-    fig = plt.figure(figsize=(7.4, 3.0), dpi=1200)
+    fig, ax = plt.subplots(figsize=(7.4, 3.0), dpi=1200)
     sns.scatterplot(data=curva_elemento, x="Concentração (g/L)", y="Absorbância", marker="o")
     sns.lineplot(x=X.flatten(), y=y_predicted, color='red', label=f"A = {modelo_regressao_linear.coef_[0]:0.4f}·[{curva_elemento['Elemento'].iloc[0]}]")
-    plt.title(f"Curva Padrão - {curva_elemento['Elemento'].iloc[0]} - R² = {r2_ajuste:0.2%}")
-    plt.xlabel("Concentração (g/L)")
-    plt.ylabel("Absorbância")
-    plt.grid()
+    ax.set_title(f"Curva Padrão - {curva_elemento['Elemento'].iloc[0]} - R² = {r2_ajuste:0.2%}")
+    ax.set_xlabel("Concentração (g/L)")
+    ax.set_ylabel("Absorbância")
+    ax.grid()
     plt.tight_layout()
     
     return fig
